934d6e8817399c9c3e133088516e2f3b827e4cc1
Saad Khalid <saad.khalid@inboxhealth.com>
2024-09-15 15:15:22 +0200

    added data spliting and infection script fixed airflow errors

--- airflow/dags/data_ingestion.py
+++ airflow/dags/data_ingestion.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import logging
+import os
+import random
 from datetime import datetime
 from datetime import timedelta
 
@@ -8,36 +10,47 @@ import pandas as pd
 
 from airflow.decorators import dag
 from airflow.decorators import task
+from airflow.exceptions import AirflowSkipException
 from airflow.utils.dates import days_ago
+
+RAW_DATA_PATH = "data/raw_data"
 
 
 @dag(
-    dag_id="ingest_data",
+    dag_id="data_ingestion",
     description="Ingest data from a file to another DAG",
     tags=["dsp", "data_ingestion"],
-    schedule=timedelta(minutes=2),
-    start_date=days_ago(n=0, hour=1),  # sets the starting point of the DAG
+    schedule=timedelta(minutes=1),
+    start_date=days_ago(0),  # sets the starting point of the DAG
     max_active_runs=1,  # Ensure only one active run at a time
 )
 def ingest_data():
     @task
     def get_data_to_ingest_from_local_file() -> pd.DataFrame:
-        nb_rows = 5
-        filepath = "data/data.csv"
-        input_data_df = pd.read_csv(filepath)
-        logging.info(f"Extract {nb_rows} rows from the file {filepath}")
-        data_to_ingest_df = input_data_df.sample(n=nb_rows)
+        files = [
+            file for file in os.listdir(RAW_DATA_PATH) if file.endswith(".csv")
+        ]
+        if not files:
+            logging.info("No CSV files found in directory, skipping task.")
+            raise AirflowSkipException("No CSV files found in directory.")
+
+        selected_file = random.choice(files)
+        file_path = os.path.join(RAW_DATA_PATH, selected_file)
+        logging.info(f"Selected file: {file_path}")
+
+        data_to_ingest_df = pd.read_csv(file_path)
+
+        os.remove(file_path)
+        logging.info(f"File {file_path} has been deleted after ingestion.")
         return data_to_ingest_df
 
     @task
     def save_data(data_to_ingest_df: pd.DataFrame) -> None:
-        filepath = (
-            f'data/raw_data/{datetime.now().strftime("%Y-%M-%d_%H-%M-%S")}.csv'
-        )
+        filepath = f'data/good_data/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
         logging.info(f"Ingesting data to the file: {filepath}")
+
         data_to_ingest_df.to_csv(filepath, index=False)
 
-    # Task relationships
     data_to_ingest = get_data_to_ingest_from_local_file()
     save_data(data_to_ingest)
 
--- app/scripts/split_data_script.py
+++ app/scripts/split_data_script.py
@@ -5,21 +5,17 @@ import pandas as pd
 
 
 def split_csv(file_path, num_files):
-    # Read the CSV file
     data = pd.read_csv(file_path)
 
-    # Limit the number of files if there are fewer rows than requested files
     num_files = min(num_files, len(data))
 
-    # Split the data into chunks
     chunks = np.array_split(data, num_files)
 
-    # Save each chunk as a separate CSV
     for i, chunk in enumerate(chunks):
         chunk.to_csv(f"data/raw_data/output_file_{i+1}.csv", index=False)
         print(f"Created output_file_{i+1}.csv")
 
 
-file_path = "data/data.csv"  # Path to your large CSV file
+file_path = "data/heart_disaease_data.csv"  # Path to your large CSV file
 num_files = 10  # Set how many files you want
 split_csv(file_path, num_files)
